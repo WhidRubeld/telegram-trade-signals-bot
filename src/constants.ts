dd05d456d4c302ae9ff4c83b99fdeb35d76e3c5b
WhidRubeld <whidrubeld@gmail.com>
2021-06-16 17:59:51 +0300

    upgrade pairs

--- src/constants.ts
+++ src/constants.ts
@@ -4,164 +4,58 @@ export type PairType = {
 }
 
 export const Pairs: PairType[] = [
+  //
   {
-    name: 'BNB/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-bnb-btc'
-  },
-  {
-    name: 'TRX/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-bnb-btc'
-  },
-  {
-    name: 'XRP/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-xrp-btc'
-  },
-  {
-    name: 'ETH/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-eth-btc'
-  },
-  {
-    name: 'BTC/USDT',
-    uri: 'https://ru.investing.com/indices/investing.com-btc-usd'
-  },
-  {
-    name: 'EOS/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-eos-btc'
-  },
-  {
-    name: 'LINK/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-link-btc'
-  },
-  {
-    name: 'ONT/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-ont-btc'
+    name: 'ADA/ETH',
+    uri: 'https://ru.investing.com/indices/investing.com-ada-eth'
   },
   {
     name: 'ADA/BTC',
     uri: 'https://ru.investing.com/indices/investing.com-ada-btc'
   },
   {
-    name: 'ETH/USDT',
-    uri: 'https://ru.investing.com/indices/investing.com-eth-usd'
-  },
-  {
-    name: 'ETC/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-etc-btc'
-  },
-  {
-    name: 'XLM/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-xlm-btc'
-  },
-  {
-    name: 'XMR/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-xmr-btc'
-  },
-  {
-    name: 'NEO/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-neo-btc'
-  },
-  {
-    name: 'ATOM/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-neo-btc'
-  },
-  {
-    name: 'DASH/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-dash-btc'
-  },
-  {
-    name: 'ZEC/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-zec-btc'
-  },
-  {
-    name: 'MATIC/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-matic-btc'
-  },
-  {
-    name: 'BAT/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-bat-btc'
-  },
-  {
-    name: 'IOST/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-iost-btc'
-  },
-  {
-    name: 'QTUM/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-qtum-btc'
-  },
-  {
-    name: 'IOTA/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-iota-btc'
-  },
-  {
-    name: 'XTZ/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-xtz-btc'
-  },
-  {
-    name: 'BCH/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-bch-btc'
-  },
-  {
-    name: 'RVN/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-rvn-btc'
-  },
-  {
-    name: 'ZIL/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-zil-btc'
-  },
-  {
     name: 'DOT/BTC',
     uri: 'https://ru.investing.com/indices/investing.com-dot-btc'
+  },
+  {
+    name: 'BNB/BTC',
+    uri: 'https://ru.investing.com/indices/investing.com-bnb-btc'
+  },
+  {
+    name: 'ETH/BTC',
+    uri: 'https://ru.investing.com/indices/investing.com-eth-btc'
   },
   {
     name: 'THETA/BTC',
     uri: 'https://ru.investing.com/indices/investing.com-theta-btc'
   },
   {
-    name: 'OMG/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-omg-btc'
+    name: 'LTC/BTC',
+    uri: 'https://ru.investing.com/indices/investing.com-xmr-eth'
   },
   {
-    name: 'DOGE/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-doge-btc'
-  },
-  {
-    name: 'DOGE/ETH',
-    uri: 'https://ru.investing.com/indices/investing.com-doge-eth'
-  },
-  {
-    name: 'UNI/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-uni-btc'
-  },
-  {
-    name: 'XRP/ETH',
-    uri: 'https://ru.investing.com/indices/investing.com-xrp-eth'
-  },
-  {
-    name: 'BNB/ETH',
-    uri: 'https://ru.investing.com/indices/investing.com-bnb-eth'
-  },
-  {
-    name: 'TRX/ETH',
-    uri: 'https://ru.investing.com/indices/investing.com-trx-eth'
-  },
-  {
-    name: 'LINK/ETH',
-    uri: 'https://ru.investing.com/indices/investing.com-link-eth'
-  },
-  {
-    name: 'EOS/ETH',
-    uri: 'https://ru.investing.com/indices/investing.com-eos-eth'
+    name: 'XRP/BTC',
+    uri: 'https://ru.investing.com/indices/investing.com-xrp-btc'
   },
   {
     name: 'ADA/ETH',
     uri: 'https://ru.investing.com/indices/investing.com-ada-eth'
   },
   {
-    name: 'XMR/ETH',
-    uri: 'https://ru.investing.com/indices/investing.com-xmr-eth'
+    name: 'DOGE/ETH',
+    uri: 'https://ru.investing.com/indices/investing.com-doge-eth'
   },
   {
-    name: 'LTC/BTC',
-    uri: 'https://ru.investing.com/indices/investing.com-xmr-eth'
-  }
+    name: 'TRX/ETH',
+    uri: 'https://ru.investing.com/indices/investing.com-trx-eth'
+  },
+  {
+    name: 'ETH/USDT',
+    uri: 'https://ru.investing.com/indices/investing.com-eth-usd'
+  },
+  {
+    name: 'BTC/USDT',
+    uri: 'https://ru.investing.com/indices/investing.com-btc-usd'
+  },
+  { name: 'SOL/BTC', uri: 'https://ru.investing.com/crypto/solana/sol-btc' }
 ]
