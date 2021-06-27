afaeaec67f85c7520a34d0872ffe038da9e38142
WhidRubeld <whidrubeld@gmail.com>
2021-06-27 18:51:04 +0300

    hotfix cron init

--- src/index.ts
+++ src/index.ts
@@ -79,6 +79,6 @@ function scheduleCallback(): void {
     })
 }
 
-scheduleCallback()
+// scheduleCallback()
 
-// cron.schedule('*/5 * * * *', scheduleCallback)
+cron.schedule('*/5 * * * *', scheduleCallback)
