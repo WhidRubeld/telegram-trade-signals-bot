6c45c9412eac561b739f374401f2737f7a0d0ccf
WhidRubeld <whidrubeld@gmail.com>
2021-06-27 17:48:05 +0300

    upgrade activity conclusion reducing

--- src/analyst.ts
+++ src/analyst.ts
@@ -2,13 +2,29 @@ import { IConclusion, IPairResponse } from './interfaces'
 import _ from 'lodash'
 import { Conclusions } from './enum'
 
+enum ActivityDirection {
+  UP,
+  DOWN
+}
 export class Analyst {
+  static activityReduce(value: number[], direction: ActivityDirection) {
+    return value.reduce((acc: number, v: number) => {
+      return direction === ActivityDirection.UP
+        ? v > 0
+          ? acc + 1
+          : acc
+        : v < 0
+        ? acc + 1
+        : acc
+    })
+  }
+
   static activityConclusion(response: IPairResponse): IConclusion {
     for (const [key, value] of Object.entries(response)) {
-      if (value.every((el: number) => el > 0)) {
+      if (this.activityReduce(value, ActivityDirection.UP) >= 4) {
         return { status: true, action: Conclusions.BUY }
       }
-      if (value.every((el: number) => el < 0)) {
+      if (this.activityReduce(value, ActivityDirection.DOWN) >= 4) {
         return { status: true, action: Conclusions.SELL }
       }
     }
